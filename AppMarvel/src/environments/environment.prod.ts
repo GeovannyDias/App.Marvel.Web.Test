50829912c9a543814b2cb45c2d251792b994a083
GeovannyDias <geova_jd@hotmail.com>
2022-02-21 22:37:41 -0500

    Build

--- AppMarvel/src/environments/environment.prod.ts
+++ AppMarvel/src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
+const AUTH_KEY = '?ts=1&apikey=066e43e68b66afde620f4285e9c3e1bc&hash=f5dc56bad5a3590ee783bdcc61cac29c'; // Switch to environment variables
+
 export const environment = {
-  production: true
+  production: true,
+  serverDomain: 'https://gateway.marvel.com',
+  port: ':443',
+  authKey: AUTH_KEY,
 };
